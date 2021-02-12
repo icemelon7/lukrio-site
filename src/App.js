41133c685300a94583afa0cff6a35dade518b17e
Kevin Zhu <kevinzhu822@gmail.com>
2021-02-11 18:40:40 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import Logo from "./assets/logo.png";
+import Mockup from "./assets/mockup.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+      <img className="Header--logo__image" src={Logo}/>
+      <div className="Container">
+        <div className="Main--text">
+          <h1 className="Main--text__title">Connecting a new generation of investors</h1>
+          <span className="Main--text__subtitle">
+          A new online investing community platform, aimed at bringing together retail investors new and old, regardless of experience level.
+          </span>
+        </div>
+        <img className="Main--mockup__image" src={Mockup}/>
+      </div>
     </div>
   );
 }
