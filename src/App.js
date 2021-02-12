cf8d72c230252bf799dac0bc9e166667314ad306
Kevin Zhu <kevinzhu822@gmail.com>
2021-02-12 01:47:24 -0800

    finished feature icons + demo states

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import React from 'react';
 import HeroImage from "./HeroImage.js"
+import FeatureIconDisplay from "./FeatureIcon.js"
 
 function App() {
   return (
     <div className="LandingPage">
       <HeroImage></HeroImage>
+      <FeatureIconDisplay></FeatureIconDisplay>
     </div>
   );
 }
-
-
 
 export default App;
