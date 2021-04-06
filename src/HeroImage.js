23b06c8f2af202b4b5119dc027ead6b974166b56
Kevin Zhu <kevinzhu822@gmail.com>
2021-04-05 23:08:38 -0700

    everything except hero is mobile

--- src/HeroImage.js
+++ src/HeroImage.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Lottie from 'react-lottie'
 import Logo from "./assets/logo.png";
 import Mockup from "./assets/mockup.png";
+import animation from "./assets/animations/top-graphic-text.json"
 import "./HeroImage.css";
 
 class HeroImage extends React.Component {
@@ -15,31 +17,33 @@ class HeroImage extends React.Component {
       this.onChange = this.onChange.bind(this);
     }
     render() {
+      const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animation,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      };
+
       return <div className="Main">
-      <div className="Header--container">
-        <img className="Header--logo__image" src={Logo}/>
-        <span className="Header--spacer"></span>
-      </div>
-      <div className="Main--container">
-        <div className="Main--text">
-          <h1 className="Main--text__title">Creating a new generation of investors</h1>
-          <span className="Main--text__subtitle">
-          A new online investing community platform, aimed at bringing together retail investors new and old, regardless of experience level.
-          </span>
-          <br/>
-          <div className="Main--form__container">
-            <input type="text" className="Main--form__input rounded" aria-label="Email" placeholder="Email Address" value={this.state.email} onChange={this.onChange}></input>
-            <button type="button"
-             className="btn btn-success Main--form__button"
-             onClick={!this.state.isLoading ? this.onSubmit : null}
-             disabled={this.state.isLoading}
-             >{this.state.isLoading ? 'Sending...' : 'Sign up for the beta'}
-             </button>
-          </div>
-          <div className="Main--form__postsubmit" hidden={!this.state.submitted}>Thanks, we'll be in touch!</div>
-        </div>
+                <Lottie options={defaultOptions}></Lottie>
+                <img className="Header--logo__image" src={Logo}/>
+                <div className="Main--form__group">
+                  <div className="Main--form__container">
+                    <input type="text" className="Main--form__input rounded" aria-label="Email" placeholder="Email Address" value={this.state.email} onChange={this.onChange}></input>
+                    <button type="button"
+                    className="btn btn-success Main--form__button"
+                    onClick={!this.state.isLoading ? this.onSubmit : null}
+                    disabled={this.state.isLoading}
+                    >{this.state.isLoading ? 'Sending...' : 'Sign up for the beta'}
+                    </button>
+                  </div>
+                  <div className="Main--form__postsubmit" hidden={!this.state.submitted}>Thanks, we'll be in touch!</div>
+                </div>
+      {/* <div className="Main--container">
         <img className="Main--mockup__image" src={Mockup}/>
-      </div>
+      </div> */}
     </div>;
     }
 
--- src/SideBySideFlipped.js
+++ src/SideBySideFlipped.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './SideBySide.css';
+import './SideBySideFlipped.css';
 
 class SideBySideFlipped extends React.Component {
     render() {
-        return (<div className="SideBySide--container">
-            <div className="SideBySide--wrapper">
+        return (<div className="SideBySide--container--flipped">
+            <div className="SideBySide--wrapper--flipped">
                 <img className="SideBySide--image__container--flipped" src={this.props.image}/>
                 <div className="SideBySide--spacer__flipped"></div>
-                <div className="SideBySide--text">
-                    <div className="SideBySide--text__header">{this.props.header}</div>
-                    <div className="SideBySide--text__main">{this.props.text}</div>
+                <div className="SideBySide--text--flipped">
+                    <div className="SideBySide--text__header--flipped">{this.props.header}</div>
+                    <div className="SideBySide--text__main--flipped">{this.props.text}</div>
                 </div>
             </div>
+            <div className="Spacer"></div>
+
         </div>
         )
     }
