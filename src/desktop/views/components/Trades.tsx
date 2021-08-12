bd145c87dcc2e34a816227b3b07e88a518d945a2
Raymond Mason <dmonyar@wharton.upenn.edu>
2021-08-12 09:14:35 -0400

    Analytics Selector Row

--- src/desktop/views/components/Trades.tsx
+++ src/desktop/views/components/Trades.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { formatRoundUSD } from '../../../util/functions';
 import './Trades.styles.css';
 import Select from 'react-select';
@@ -6,19 +6,96 @@ import Switch from 'react-switch';
 import Bull from '../../../assets/analytics/bull.png';
 import Bear from '../../../assets/analytics/bear.png';
 import { LightMode } from '../../../Colors';
-export type trades = {
-
+import Header from './Header';
+export type tradeStats = {
+    _id: string;
+    lA: number;
+    sA: number;
+    lV: number;
+    sV: number;
+    lU: string[];
+    sU: string[];
+    lN: number;
+    sN: number;
 }
+
+const rowStyles = {
+    first: {
+        width: '20%',
+        height: '5%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    second: {
+        width: '15%',
+        height: '5%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    third: {
+        width: '25%',
+        height: '5%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    fourth: {
+        width: '15%',
+        height: '5%',
+        display: 'flex',
+        alignItems: 'center'
+    },
+    fifth: {
+        width: '25%',
+        height: '5%',
+        display: 'flex',
+        alignItems: 'center'
+    }
+};
 
 const Trades = () => {
 
     const [isBear, setIsBear] = useState<boolean>(false);
-    const [sort, setSort] = useState<string>("Amount");
+    const [sortCol, setSortCol] = useState<string>("Symbol");
+    const [isSortDown, setIsSortDown] = useState<boolean>(true);
     const [total, setTotal] = useState<number>(0);
     const [loading, setLoading] = useState<boolean>(true);
     const [period, setPeriod] = useState<string>("Today");
-    const [data, setData] = useState<trades[]>([]);
+    const [data, setData] = useState<tradeStats[]>([]);
     const [type, setType] = useState<string>("Amount");
+
+    const _handleSelectorPress = (col : string) =>  {
+        // changed column, default sort decreasing
+        if (col !== sortCol) {
+          if (col === 'Symbol') {
+            //sort alphabetically
+            setData(data.sort((a, b) => a._id.localeCompare(b._id)));
+          } else if (col === 'Bull $') {
+            setData(data.sort((a, b) => b.lV - a.lV));
+          } else if (col === 'Bull Trades') {
+            setData(data.sort((a, b) => b.lA - a.lA));
+          } else if (col === 'Bull Users') {
+            setData(data.sort((a, b) => b.lN - a.lN));
+          } else if (col === 'Bear $') {
+            setData(data.sort((a, b) => b.sV - a.sV));
+          } else if (col === 'Bull Trades') {
+            setData(data.sort((a, b) => b.sA - a.sA));
+          } else if (col === 'Bull Users') {
+            setData(data.sort((a, b) => b.sN - a.sN));
+          }
+          setSortCol(col);
+          setIsSortDown(true);
+        } else {
+          //same column, so invert order
+          let temp = data;
+          setData(temp.reverse());
+          if (isSortDown) {
+            // used to be sorted decreasing,
+            setIsSortDown(false);
+          } else {
+            setIsSortDown(true);
+          }
+        }
+    }
 
     return (
         <>
@@ -58,7 +135,11 @@ const Trades = () => {
                 </div>
             </div>
             <div className="selector-row">
-                
+                <Header text="Symbol" style={rowStyles.first} sortCol={sortCol} isSortDown={isSortDown} handle={_handleSelectorPress}/>
+                <Header text="Bull $" style={rowStyles.second} sortCol={sortCol} isSortDown={isSortDown} handle={_handleSelectorPress}/>
+                <Header text={type === 'Users' ? 'Bull Users' : 'Bull Trades'} style={rowStyles.third} sortCol={sortCol} isSortDown={isSortDown} handle={_handleSelectorPress}/>
+                <Header text="Bear $" style={rowStyles.fourth} sortCol={sortCol} isSortDown={isSortDown} handle={_handleSelectorPress}/>
+                <Header text={type === 'Users' ? 'Bear Users' : 'Bear Trades'} style={rowStyles.fifth} sortCol={sortCol} isSortDown={isSortDown} handle={_handleSelectorPress}/>
             </div>
             <div className="table">
                 {
