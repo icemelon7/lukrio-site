15c049499797c2f4177cf96384bf4665ce3f5d9c
Raymond Mason <dmonyar@wharton.upenn.edu>
2021-09-03 11:14:50 -0400

    HTTPS

--- src/credentials.ts
+++ src/credentials.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'http://3.143.1.122:8080';
+export const SERVER_URL = 'https://3.143.1.122:8081';
 export const token = "9ej5xzwwY7TpmC6GHaLy8LwhHDEtjbymQdz4f4ZAZHVVEsGzzMGeamxkwvGgycsz";
